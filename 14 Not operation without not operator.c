b5168862174d222de485eacfb60ed9921defcfc3
rk <englishenglishenglish@proton.me>
2025-04-14 15:30:29 +0000

    Add files via upload

---
+++ 14 Not operation without not operator.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+    int n;
+
+    scanf("%d",&n); 
+    bool num1[n];
+    
+    
+
+    for(int i=0;i<n;i++){
+        scanf("%d",(int*)&num1[i]);
+    }
+
+
+
+    for(int k=0;k<n;k++){
+        if(num1[k]==0){
+            num1[k]=1;
+        }else{
+            num1[k]=0;
+        }
+    }
+
+    for(int l=0;l<n;l++){
+        printf("%d ",num1[l]);
+    }
+
+
+
+
+
+
+
+    return 0;
+}
