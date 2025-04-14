e646bfa193a65f028078981ebad4e39402dd989d
rk <englishenglishenglish@proton.me>
2025-04-14 15:27:59 +0000

    Add files via upload

---
+++ 10 And_or_not operation of two integers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+        int num1,num2;
+        scanf("%d",&num1);
+        scanf("%d",&num2);
+    
+
+        int or_res=num1|num2;
+        int and_res=num1&num2;
+        int not_res=~num1;
+
+        printf("%d\n",or_res);
+        printf("%d\n",and_res);
+        printf("%d\n",not_res);
+
+
+
+
+
+
+    return 0;
+}
