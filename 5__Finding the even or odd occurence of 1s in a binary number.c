66608e404f0f14048acac900172f561fa4fbc56a
SK <englishenglishenglish@proton.me>
2025-04-30 14:33:02 +0000

    Add files via upload

---
+++ 5__Finding the even or odd occurence of 1s in a binary number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+        int xor_res=0;
+        int n=32;
+        int arr[n];
+        printf("give a binary number in 32 bits(0s or 1s):\n");
+
+        for(int i=0;i<n;i++){
+            scanf("%d",&arr[i]);
+            if(arr[i]!=0&&arr[i]!=1){
+                printf("the wrong input,please give only 0 or 1 \n");
+                return -1;
+            }
+
+            xor_res=xor_res^arr[i];
+        }
+
+        if(xor_res==0){
+            printf("the set bits have in even times\n");
+        }else{
+            printf("the set bits have in odd times\n");
+        }
+
+
+
+
+
+
+
+
+
+    return 0;
+}
